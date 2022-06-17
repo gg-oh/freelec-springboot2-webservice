ca460c649689568cb18fd23d44caa5e65e29f9d0
gg-oh <gyeong9768@gmail.com>
2022-06-17 22:03:53 +0900

    feat: 게시글 등록, 수정, 삭제, 조회 기능 추가

---
+++ src/main/java/dev/ggoh/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package dev.ggoh.springboot.web.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import dev.ggoh.springboot.domain.posts.Posts;
+import lombok.Getter;
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
