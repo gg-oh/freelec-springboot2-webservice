05312fe5cab1bbc2727b29749dc3bc865609bfa6
gg-oh <gyeong9768@gmail.com>
2022-06-17 16:03:15 +0900

    feat: 게시글 등록, 수정, 조회 API 작성

---
+++ src/main/java/dev/ggoh/springboot/service/posts/PostsService.java
@@ -0,0 +1,38 @@
+package dev.ggoh.springboot.service.posts;
+
+import dev.ggoh.springboot.domain.posts.Posts;
+import dev.ggoh.springboot.domain.posts.PostsRepository;
+import dev.ggoh.springboot.web.dto.PostsResponseDto;
+import dev.ggoh.springboot.web.dto.PostsSaveRequestDto;
+import dev.ggoh.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
