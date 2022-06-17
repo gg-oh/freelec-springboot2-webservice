206ceaba51de9f38285f0965a53460f1a7561d35
gg-oh <gyeong9768@gmail.com>
2022-06-17 14:12:38 +0900

    feat: lombok 적용 및 dto 추가 테스트

---
+++ src/test/java/dev/ggoh/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package dev.ggoh.springboot.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombok_function_test() {
+        //given
+        String name = "Test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
